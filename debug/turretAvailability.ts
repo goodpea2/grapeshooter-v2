8c461a32da6f64c493a55b5e8c9b34f5446b59e3
goodp2 <83803292+goodpea2@users.noreply.github.com>
2026-02-19 21:38:34 +0700

    feat: Enable t3_triplepea turret
    Moves the 't3_triplepea' turret from the disabled list to the enabled list. This change makes the turret available for use in the game.

--- debug/turretAvailability.ts
+++ debug/turretAvailability.ts
@@ -4,12 +4,13 @@ export const EnabledTurrets: string[] = [
   't2_repeater', 't2_firepea', 't2_laser2', 't2_peanut', 't2_puncher', 't2_tall', 't2_mortar', 
   't2_pulse', 't2_laserexplode', 't2_minespawner', 't2_snowpea', 't2_iceray', 't2_spike', 
   't2_icebomb', 't2_stun',
-  't3_triplepea', 't3_repulser', 't3_bowling', 't3_snowpeanut', 't3_skymortar',
-  't3_puncher2', 't3_aoelaser', 't3_iceray2', 't3_miningbomb', 't3_tesla', 't3_icepuncher',
-  't3_densnut', 't3_durian', 't3_spike2', 't3_holonut'
+  't3_triplepea'
 ];
 
 export const DisabledTurrets: string[] = [
+  't3_repulser', 't3_bowling', 't3_snowpeanut', 't3_skymortar',
+  't3_puncher2', 't3_aoelaser', 't3_iceray2', 't3_miningbomb', 't3_tesla', 't3_icepuncher',
+  't3_densnut', 't3_durian', 't3_spike2', 't3_holonut',
   't3_firepea2', 't3_spinnut', 't3_mortar2', 't3_snowpea2', 't3_inferno',
   't3_flamethrower', 't3_laser3', 't3_minefield', 't3_frostfield', 't3_triberg'
 ];
