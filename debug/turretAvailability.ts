8d39a281acc13dca4ab197480d4ef354cc7b1ad8
goodp2 <83803292+goodpea2@users.noreply.github.com>
2026-02-26 01:29:15 +0700

    feat: Add new turrets and update version
    Introduces new 't2_tall' and 't2_puncher' turrets to the debug availability list.
    Updates the build version in constants.ts.

--- debug/turretAvailability.ts
+++ debug/turretAvailability.ts
@@ -1,9 +1,9 @@
 
 export const EnabledTurrets: string[] = [
   't_pea', 't_laser', 't_wall', 't_mine', 't_ice', 't_sunflower', 't_seed', 't_lilypad',
-  't2_repeater', 't2_firepea', 't2_laser2', 't2_peanut', 't2_mortar', 
+  't2_repeater', 't2_firepea', 't2_laser2', 't2_peanut', 't2_tall', 't2_mortar', 
   't2_pulse', 't2_laserexplode', 't2_minespawner', 't2_snowpea', 't2_iceray', 't2_spike', 
-  't2_icebomb', 't2_stun',
+  't2_icebomb', 't2_stun', 't2_puncher',
   't3_triplepea', 't3_repulser', 't3_bowling', 't3_snowpeanut', 't3_skymortar',
   't3_puncher2', 't3_aoelaser', 't3_iceray2', 't3_miningbomb', 't3_tesla', 't3_icepuncher',
   't3_densnut', 't3_durian', 't3_spike2', 't3_holonut',
